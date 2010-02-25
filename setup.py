6d1057134a99e376243c9033615bb6ff9bdecbc4
Mark van Lent <m.van.lent@zestsoftware.nl>
2010-02-25 21:27:58 +0100

    Back to using no accent.
    Sorry Vincent, but I could not find a way to trick both "upload" and "register" into accepting your last name.

--- setup.py
+++ setup.py
@@ -2,51 +2,16 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-version = '0.1'
-
-
-class UltraMagicString(object):
-    """ Catch-22:
-    - if I return Unicode, python setup.py --long-description as well
-      as python setup.py upload fail with a UnicodeEncodeError
-    - if I return UTF-8 string, python setup.py sdist register
-       fails with an UnicodeDecodeError
-
-    Taken from zest.releaser.
-    Adapted from http://stackoverflow.com/questions/1162338
-    """
-
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        return self.value
-
-    def __unicode__(self):
-        return self.value.decode('utf-8')
-
-    def __add__(self, other):
-        return UltraMagicString(self.value + str(other))
-
-    def split(self, *args, **kw):
-        return self.value.split(*args, **kw)
-
-
-def read(filename):
-    return unicode(codecs.open(filename, encoding='utf-8').read())
-
-
-long_description = u'\n\n'.join([read('README.txt'),
-                                 read(os.path.join("docs", "CREDITS.txt")),
-                                 read(os.path.join("docs", "HISTORY.txt")),
-                                 read(os.path.join("docs", "TODO.txt")),
-                                 ])
-
+version = '0.1.1'
 
 setup(name='mr.inquisition',
       version=version,
       description="A package to help with exploring a Plone site.",
-      long_description=long_description,
+      long_description=(
+          open("README.txt").read() + "\n" +
+          open(os.path.join("docs", "CREDITS.txt")).read() + "\n\n" +
+          open(os.path.join("docs", "HISTORY.txt")).read() + "\n\n" +
+          open(os.path.join("docs", "TODO.txt")).read()),
       classifiers=[
         "Intended Audience :: Developers",
         "Framework :: Plone",
