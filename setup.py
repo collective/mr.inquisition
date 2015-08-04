c268136a83fd093f7eac3f82d46b528cf14c8d84
Maurits van Rees <maurits@vanrees.org>
2015-08-04 12:36:50 +0200

    Improved setup.py.
    Complient version number.
    More classifiers.
    Added keywords.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = '0.2dev'
+version = '0.2.dev0'
 
 long_description = (
     read('README.rst')
@@ -20,12 +20,20 @@ setup(name='mr.inquisition',
       description="A package to help with exploring a Plone site.",
       long_description=long_description,
       classifiers=[
-        "Intended Audience :: Developers",
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='',
+          "Intended Audience :: Developers",
+          "Framework :: Plone",
+          "Framework :: Plone :: 3.3",
+          "Framework :: Plone :: 4.0",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Plone :: 4.3",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.4",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          ],
+      keywords='information objects plone',
       author='Mark van Lent',
       author_email='mark@vlent.nl',
       url='https://github.com/collective/mr.inquisition',
